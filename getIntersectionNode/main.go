aa8f11dfb7efb1bb549c7671d584f3364add9ea8
吴洲洋 <w910820618@163.com>
2021-07-18 09:55:49 +0800

    getIntersectionNode

---
+++ getIntersectionNode/main.go
@@ -0,0 +1,34 @@
+package main
+
+func main() {
+
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	if headA == nil || headB == nil {
+		return nil
+	}
+
+	nodeA := headA
+	nodeB := headB
+
+	for nodeA != nodeB {
+		if nodeA == nil {
+			nodeA = headB
+		} else {
+			nodeA = nodeA.Next
+		}
+		if nodeB == nil {
+			nodeB = headA
+		} else {
+			nodeB = nodeB.Next
+		}
+	}
+
+	return nodeA
+}
